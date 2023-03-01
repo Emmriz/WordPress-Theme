60283ded5141a47c4a3c859bfa33e57de2e7f891
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2023-03-01 05:19:40 +0100

    added: How to call a post loop in wordpress theme

--- wp-content/themes/awesometheme/index.php
+++ wp-content/themes/awesometheme/index.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
-<h1>This is my index</h1>
+<?php 
+    if(have_posts()):
+
+         while( have_posts() ): the_post(); ?>
+
+        <h3> <?php the_title(); ?></h3>
+         <p><?php the_content();?></p>
+         <small>Posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a')?> in <?php the_category();?></small>
+
+         <hr>
+
+       <?php endwhile;
+
+    endif;
+
+?>
 
 <?php get_footer(); ?>
